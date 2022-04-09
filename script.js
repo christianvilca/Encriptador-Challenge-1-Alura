97c6e05940d37d6fe8281a4df0e94fb0997fe9fe
christianvilca <christian.vilcaq@gmail.com>
2022-04-08 20:40:57 -0500

    encripta y desencriptar

--- script.js
+++ script.js
@@ -18,3 +18,58 @@ No se permite acentuación de palabras
 Solo letras minusculas
 No se permite acentuación de palabras   
 */
+
+const btnEncrypt = document.getElementById("btn-encriptar")
+const btnDecrypt = document.getElementById("btn-desencriptar")
+const btnCopy = document.getElementById("btn-copy")
+const textEncript = document.getElementById("input-texto")
+const textMsg = document.getElementById("msg")
+const labelError = document.getElementById("label-error")
+
+textEncript.focus();
+
+btnEncrypt.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  validation()
+
+  textMsg.value = textEncript.value.toLowerCase()
+    .replace(/e/g,"enter")
+    .replace(/i/g,"imes")
+    .replace(/a/g,"ai")
+    .replace(/o/g,"ober")
+    .replace(/u/g,"ufat")
+
+})
+
+btnDecrypt.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  validation()
+
+  textMsg.value = textEncript.value
+    .replace(/enter/g,"e")
+    .replace(/imes/g,"i")
+    .replace(/ai/g,"a")
+    .replace(/ober/g,"o")
+    .replace(/ufat/g,"u")
+
+})
+
+function validation(){
+  labelError.classList.add("invisible")
+
+  if(!textEncript.value.match(/^[A-Za-z]+$/g)) {
+    labelError.classList.remove("invisible")
+    console.log("first")
+    return;
+  }
+}
+
+btnCopy.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  textMsg.select();
+  document.execCommand("copy");
+
+})
